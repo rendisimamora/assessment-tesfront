536ec3b7724d9c2a2b534f9814cb3b1cef064d24
rendisimamora <rendisimamora7127@gmail.com>
2020-01-26 01:23:03 +0700

    style: add section reviews

---
+++ components/blocks/SectionReviews.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="my-5 d-flex justify-space-between">
+    <div class="item">
+      <H2 title="Latest Reviews" />
+      <div class="mb-3 d-flex justify-space-between">
+        <p class="subheading secondary--text">
+          So you can make better purchase decision
+        </p>
+
+        <a href="#" class="subheading text-none primary--text">
+          See more
+          <v-icon class="icon primary--text">keyboard_arrow_right</v-icon>
+        </a>
+      </div>
+
+      <flickity ref="flickity" :options="flickityOptions">
+        <CardReviews
+          v-for="(item, i) in items"
+          :key="i"
+          :productImage="item.product.image"
+          :productName="item.product.name"
+          :productDescription="item.product.desc"
+          :userName="item.user"
+          :userProfile="item.profile"
+          :star="item.star"
+          :comment="item.comment"
+        />
+      </flickity>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array
+    }
+  },
+
+  data() {
+    return {
+      flickityOptions: {
+        contain: true,
+        prevNextButtons: false,
+        pageDots: true,
+        wrapAround: true,
+        fullscreen: true
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.item {
+  width: 100%;
+}
+
+.is-selected {
+  width: 50%;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+}
+</style>
---
+++ components/global/CardReviews.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="card">
+    <div class="card__reviews pa-5">
+      <div class="d-flex">
+        <img
+          class="card__reviews-image"
+          :src="productImage"
+          :alt="productName"
+        />
+        <div class="card__name ml-2 d-flex justify-center flex-column">
+          <p class="font-weight-bold">{{ productName }}</p>
+          <p class="font-weight-medium">{{ productDescription }}</p>
+        </div>
+      </div>
+      <v-divider class="my-2" />
+      <div class="d-flex justify-space-between">
+        <v-rating
+          class="mr-1"
+          :empty-icon="'mdi-star'"
+          :background-color="'secondary'"
+          v-model="star"
+          half-increments
+          readonly
+        ></v-rating>
+        <p class="caption secondary--text">2 Hours ago</p>
+      </div>
+      <p class="body-2 mt-2">
+        {{ comment }}
+        <span class="primary--text">Read More</span>
+      </p>
+    </div>
+    <div class="card__profile text-center">
+      <img
+        class="card__profile-image image-circle"
+        src="https://static.femaledaily.com/dyn/50/images/user-pics/ea7cb60774b63df3a0a084906221ea4b.jpg"
+        :alt="userName"
+      />
+      <p class="body-2 font-weight-bold">{{ userName }}</p>
+      <p class="caption secondary--text">
+        <span>
+          {{ userProfile.join(", ") }}
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    productName: String,
+    productDescription: String,
+    productImage: String,
+    star: Number,
+    comment: String,
+    userName: String,
+    userProfile: Array
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 50%;
+  margin-right: 10px;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+  &__reviews {
+    border: solid 1px #e0e0e0;
+    border-radius: 8px;
+    &-image {
+      width: 80px;
+      height: 80px;
+      object-fit: cover;
+    }
+  }
+  &__profile {
+    margin-top: -15px;
+    &-image {
+      width: 50px;
+      height: 50px;
+      object-fit: cover;
+    }
+  }
+}
+</style>
