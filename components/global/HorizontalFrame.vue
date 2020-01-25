1761aa84b8d577b9d130a077daa09d52bf7cff75
rendisimamora <rendisimamora7127@gmail.com>
2020-01-26 01:22:48 +0700

    style: add component adsense

--- components/global/HorizontalFrame.vue
+++ components/global/HorizontalFrame.vue
@@ -3,7 +3,7 @@
     class="frame frame-billboard d-flex align-center justify-center info my-5"
   >
     <h1 class="text-center secondary--text">
-      Horizontal 970x50 <br />
+      Horizontal 970x250 <br />
       (Internal campaign only)
     </h1>
   </div>
