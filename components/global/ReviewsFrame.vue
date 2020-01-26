97d8d1d5df256d95514471b3cdc97dfe8daf428a
rendisimamora <rendisimamora7127@gmail.com>
2020-01-27 00:22:03 +0700

    style: update responsive frame adsense

--- components/global/ReviewsFrame.vue
+++ components/global/ReviewsFrame.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="frame-reviews d-flex align-center justify-center info">
-    <h1 class="text-center secondary--text">MR 2 300x25</h1>
+    <h1 class="text-center secondary--text">MR 2 300x250</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.frame {
+  &-reviews {
+    @media (max-width: 899px) {
+      margin: 40px auto 0px auto;
+    }
+  }
+}
+</style>
